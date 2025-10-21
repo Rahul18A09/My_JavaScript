2924f17d84a7c9e1a81a8134751e6d38d3566258
Rahul18A09 <147308572+Rahul18A09@users.noreply.github.com>
2025-10-21 09:01:18 +0530

    Function Practice

---
+++ Fuctions/script.js
@@ -0,0 +1,145 @@
+
+//(1)
+// function greet(name){               
+//     return "Hello, " + name + "!";
+// }
+// console.log(greet("Rahul"));
+// console.log(greet("Student"));
+
+
+//(2)
+function add(a , b) {
+    return a + b ;
+}
+let result = add (5, 7);
+console.log("Sum is:", result);
+
+
+//(3)
+// const multiply  = function ( x, y) {
+//     return x * y ;
+// };
+// console.log(multiply(4 , 3));
+
+
+//(4)
+// function greetUser(name = "Guest") {
+//     return "Welcome, " + name;
+// }
+
+// console.log(greetUser("Rahul"));
+// console.log(greetUser());
+
+
+//(5)
+// function myFunctions ( a , b ) {
+//     return a * b ;
+// }
+
+//  let ans = myFunctions ( 4 , 3);
+// console.log(myFunctions(4 , 3));
+
+
+//(6)
+//   function multiply (a, b) {
+//   return a * b ;
+// }
+
+// let x = multiply (5 , 3 );
+// console.log(x);
+
+
+//(7)
+// function sayMyName () {
+//     console.log("Rahul"); 
+// }
+// sayMyName ();
+
+
+//(8)
+// function printCounting(){
+//   for (let i=1; i<=100; i++){
+//     console.log(i);
+//   }
+// }
+
+// printCounting();
+   
+
+//(9)
+// function getAverage(num1, num2){
+//     let avg = (num1 + num2)/2;
+//     console.log("Average: ", avg);
+    
+// }
+
+// getAverage(3, 70);
+
+
+// <== return functions ==>
+
+
+//(10)
+// function getSum(a,b,c){
+//     let sum = a + b + c;
+//     return sum;
+// }
+
+// let ans  = getSum(1, 2, 3);
+// console.log("Printing sum: ", ans);
+
+
+//(11)
+// function getMyName(firstname, lastname){
+// let fullname = firstname + " " + lastname;
+// return fullname;
+// // unreachable statements
+// let a = 10;
+// let b = 15;
+// let sum  = a + b;
+// console.log(sum);
+// }
+// let fullname = getMyName("Rahul" , "Bharada");
+// console.log("Full Name: ", fullname);
+
+
+//(12)
+// function getMultiplication (a , b){
+//     let ans = a*b;
+//     return ans;
+    // return a*b; // we can also write
+// }
+// let ans = getMultiplication (2,10); // store in to variable
+// console.log(ans);
+
+// console.log(getMultiplication(2,10)); // or we can direct print like this
+
+
+//(13)
+// let getMultiplication = function (a,b){
+//     return a*b;
+// }
+
+// let ans = getMultiplication (2,20);
+// console.log(ans);
+
+
+//(14)
+// let squareNumber = function(num) {
+// let ans = num**2;
+// return ans;
+// }
+
+// let ans = squareNumber (5);
+// console.log(ans);
+
+
+// <==  arrow function  ==>
+
+//(15)
+// let getExp = (x,y) => {
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2,10));
---
+++ Fuctions/script1.js
@@ -0,0 +1,28 @@
+// console.log("Hello!");
+
+// function greet(name) {
+//     return "Hello, " + name + "!";
+// }
+
+// let greet1  = greet("rahul");
+// console.log(greet1);
+
+// function add(a ,b){
+//     return a + b;
+// }
+
+// let sum = add(3, 7);
+// console.log(sum);
+
+// Arrow Function
+
+// const greet = (name) => {
+//     return "Hello, " + name + "!";
+// };
+
+// const add = greet("Asha");
+// console.log(add);
+
+// const add = (a , b) => a + b;
+
+// console.log(add);
