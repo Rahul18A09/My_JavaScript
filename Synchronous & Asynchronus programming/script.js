1b4aaf9d70cb54d23ca202c5eea5713626db28a8
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-12 17:57:45 +0530

    Update Practice

---
+++ Synchronous & Asynchronus programming/script.js
@@ -0,0 +1,39 @@
+// JavaScript Event
+
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
+
+// Javascript Callback function
+
+// function greet(name, callback) {
+//   const greetingElement = document.getElementById("greeting");
+//   greetingElement.innerHTML = "Hello" + name;
+//   callback();
+// }
+
+// function sayGoodbye() {
+//   const goodbyeElement = document.getElementById("farewell");
+//   goodbyeElement.innerHTML = "Goodbye!";
+// }
+
+// greet("Alice", sayGoodbye);
+
+
+
+// Promises
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then(data=> {
+//     console.log("Data received:", data);
+//   })
+//   .catch(error=> {
+//     console.error("There was a problem with the fetch operation:", error);
+//   });
+
