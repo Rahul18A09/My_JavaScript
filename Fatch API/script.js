76c56d9006a342f1b2c4a875f5fcf412cfc7aeaf
Rahul18A09 <rahulbharda60@gmail.com>
2025-11-21 11:31:05 +0530

    new update

--- Fatch API/script.js
+++ Fatch API/script.js
@@ -5,56 +5,56 @@
 // };
 // getProducts();
 
-// let getProducts = () => {
-//   fetch("https://dummyjson.com/products")
-//     .then((res) => res.json())
-//     .then((finalRes) => {
-//       let { products } = finalRes;
-//       console.log(products);
-//     })
+let getProducts = () => {
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((finalRes) => {
+      let { products } = finalRes;
+      console.log(products);
+    })
+}
+
+let getCategory = () => {
+  fetch("https://dummyjson.com/products/categories")
+    .then((res) => res.json())
+    .then((finalres)=> {
+        console.log(finalres);
+
+    })
+};
+
+getProducts();
+getCategory();
+
+// document.getElementById("getData").addEventListener("click", getData());
+
+// async function getData() {
+//   try {
+//     const response = await fetch("https://fakestoreapi.com/users");
+//     const data = await response.json();
+//     console.log(data);
+
+//     let tbl = "";
+//     data.map((value) => {
+//       tbl += `<tr>
+//       <td>${value.id}</td>
+//       <td>${value.name.firstname} ${value.name.lastname}</td>
+//       <td>${value.email}</td>
+//       <td>${value.password}</td>
+//       <td>${value.phone}</td>
+//       </tr>`;
+
+//     });
+
+//     document.getElementById("showdata").innerHTML = tbl;
+
+//   } catch (error) {
+//     console.log("Error fetching users:", error);
+//   }
 // }
 
-// let getCategory = () => {
-//   fetch("https://dummyjson.com/products/categories")
-//     .then((res) => res.json())
-//     .then((finalres)=> {
-//         console.log(finalres);
+// function hideData() {
+//   let data = document.getElementById("showdata").innerHTML = "";
+// }
 
-//     })
-// };
-
-// getProducts();
-// getCategory();
-
-document.getElementById("getData").addEventListener("click", getData());
-
-async function getData() {
-  try {
-    const response = await fetch("https://fakestoreapi.com/users");
-    const data = await response.json();
-    console.log(data);
-
-    let tbl = "";
-    data.map((value) => {
-      tbl += `<tr>
-      <td>${value.id}</td>
-      <td>${value.name.firstname} ${value.name.lastname}</td>
-      <td>${value.email}</td>
-      <td>${value.password}</td>
-      <td>${value.phone}</td>
-      </tr>`;
-
-    });
-
-    document.getElementById("showdata").innerHTML = tbl;
-
-  } catch (error) {
-    console.log("Error fetching users:", error);
-  }
-}
-
-function hideData() {
-  let data = document.getElementById("showdata").innerHTML = "";
-}
-
- hideData();
+//  hideData();
