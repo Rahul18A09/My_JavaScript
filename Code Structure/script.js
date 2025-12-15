143360c0e6512e9dc0bb9dca4b22282328690cd5
Rahul18A09 <rahulbharda60@gmail.com>
2025-12-15 17:14:24 +0530

    new-file-added

---
+++ Code Structure/script.js
@@ -0,0 +1,16 @@
+// alert("Hello"); alert("World");
+
+// alert("Hello");
+// alert("World");
+
+// alert(3 + 
+//     1
+//     +  2);
+
+// alert("Hello")
+// [1,2].forEach(alert);
+
+// alert("Hello")[1, 2].forEach(alert);
+
+// commnets
+
