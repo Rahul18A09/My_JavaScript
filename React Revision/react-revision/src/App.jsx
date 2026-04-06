287f9e3ab8dac23daabf88b0de6005000f7b1208
Rahul18A09 <rahulbharda60@gmail.com>
2026-04-06 13:32:37 +0530

    practice

--- React Revision/react-revision/src/App.jsx
+++ React Revision/react-revision/src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  async function getUsers() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+
+    console.log(response.data);
+  }
+
   return (
-    // <div className="demo">
-    //   <div className="count">
-    //     <h2>Count: {count}</h2>
-    //     <button onClick={() => setCount(count + 1)}>Increase</button>
-    //     <button onClick={() => setCount(count - 1)}>Decrease</button>
-    //   </div>
-    // </div>
+    <>
+      <div className="demo">
+        <div className="count">
+          <h2>Count: {count}</h2>
 
+          <button onClick={() => setCount(count + 1)}>Increase</button>
+
+          <button onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+      </div>
+
+      <div className="btn">
+        <button onClick={getUsers}>Click</button>
+      </div>
+    </>
     // Inline Eventhandler
-
-    <button onClick={() => console.log("button clicked")}>CLick</button>
   );
 }
 
