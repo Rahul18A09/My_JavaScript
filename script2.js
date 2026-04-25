d2e622866c2e505f8ad6b725e8247bcdb314b0e7
Rahul18A09 <rahulbharda60@gmail.com>
2026-04-25 23:51:37 +0530

    practice

--- script2.js
+++ script2.js
@@ -14,11 +14,48 @@
 
 // 2. Chech panlindrome
 
-function isPalindrome(str){
-    let reversed = str.split('').reverse().join('');
-    return str === reversed;
+// function isPalindrome(str){
+//     let reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
+
+// console.log(isPalindrome("level"));
+// console.log(isPalindrome("hello"));
+// console.log(isPalindrome("racecar"));
+
+// 3. Find largest number inan Array
+
+// let arr = [10, 5, 8, 20];
+
+// function maxNum(arr) {
+//     return Math.max(...arr)
+// }
+
+// console.log(maxNum(arr));
+
+
+// 4. Remove Duplicates from an array
+
+// let arr = [1,2,2,3,4,4,5];
+
+// function removeDuplicates(arr) {
+//     return [...new Set(arr)];
+// }
+
+// console.log(removeDuplicates(arr));
+
+// 5. count occurences of Characters
+
+let word = "apple";
+
+function countChars(str){
+    let obj = {};
+    for (let char of str){
+        obj[char] = (obj[char] || 0) + 1;
+    }
+
+    return obj;
 }
 
-console.log(isPalindrome("level"));
-console.log(isPalindrome("hello"));
-console.log(isPalindrome("racecar"));
+console.log(countChars(word));
+
