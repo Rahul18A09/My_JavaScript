adb10d8dfe7935c86a4b51c9718ef0c27139eee2
Rahul18A09 <rahulbharda60@gmail.com>
2026-05-03 22:51:55 +0530

    start pattern

--- script3.js
+++ script3.js
@@ -63,11 +63,28 @@
 // console.log(isPanildrome("hello"));
 // console.log(isPanildrome("level"));
 
+// let n = 4;
+
+// for (let i = 1; i <= n; i++){
+//     let row = '';
+//     for(j = 1; j <= n; j++){
+//         row += '* ';
+//     }
+
+//     console.log(row);
+    
+// }
+
+
+
+// star pattern
+
 let n = 4;
 
-for (let i = 1; i <= n; i++){
+for(let i = 1; i <= n ; i++){
     let row = '';
-    for(j = 1; j <= n; j++){
+
+    for(let j = 1; j<= n; j++){
         row += '* ';
     }
 
